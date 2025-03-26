728bc7403828402fec3bc49d9e1e269f0e201406
k3nray <k3nray@gmail.com>
2025-03-26 17:07:05 +0100

    graficas listas

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Gr-ficas-con-Star-Wars-
+[<img src="path/to/image.png">](https://github.com/TheBridge-FullStackDeveloper/temario_fullstack_FT_mar25_MAD/raw/master/assets/js_avanzado/clase9/star_wars.webp),
+
+Practicaremos cómo crear gráficas con las librerías vistas en clase
+
+Pediremos las películas de Star Wars y pintaremos una gráfica de líneas en la que podamos ver cada una de las películas.
+En el eje X el nombre de la película
+En el eje Y año de publicación
+API ENDPOINT --> https://swapi.dev/api/films/
+Pediremos los personajes de Star Wars y pintaremos una gráfica de barras en la que podamos ver
+En el eje X el nombre del personaje
+En el eje Y el número de películas en las que ha participado.
+API ENDPOINT --> https://swapi.dev/api/people/
+Para pintar todo esto usaremos Chartist Link a la docu: Chartist
---
+++ starw.js
@@ -0,0 +1,65 @@
+var data = {
+  // A labels array that can contain any sort of values
+  labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+  // Our series array that contains series objects or in this case series data arrays
+  series: [[5, 2, 4, 2, 0]],
+};
+// As options we currently only set a static size of 300x200 px
+var options = {
+  width: "600px",
+  height: "500px",
+  axisY: {
+    onlyInteger:true
+}
+  
+};
+// In the global name space Chartist we call the Line function to initialize a line chart. As a first parameter we pass in a selector where we would like to get our chart created. Second parameter is the actual data object and as a third parameter we pass in our options
+new Chartist.Line(".ct-chart", data, options);
+
+async function getmoviesInfo() {
+  let getMovies = await fetch("https://swapi.dev/api/films/");
+  let movies = await getMovies.json();
+  let title = movies.title;
+  console.log(movies);
+  return movies;
+}
+getmoviesInfo();
+getmoviesInfo().then((movies) => {
+  const labels = movies.results.map((movie) => movie.title);
+  const series = [
+    movies.results.map((movie) => parseInt(movie.release_date.split("-")[0])),
+  ];
+  const updatedData = {
+    labels: labels,
+    series: series,
+  };
+  new Chartist.Line(".ct-chart", updatedData, options);
+
+});
+
+async function getPeopleInfo() {
+  let getPeople = await fetch("https://swapi.dev/api/people/");
+  let people = await getPeople.json();
+  console.log(people);
+  return people;
+}
+getPeopleInfo();
+getPeopleInfo().then((people) => {
+    const labels = people.results.map((people) => people.name);
+    const series = [
+        people.results.map(people => people.films.length ),
+      ];
+      const barChartData = {
+        labels: labels,
+        series: series,
+      };
+        const barChartOptions={
+            width:'600px',
+            height:'500px',
+            axisY: {
+                onlyInteger:true
+        }
+      }
+      new Chartist.Bar(".cT-chart", barChartData, barChartOptions);
+
+    });
